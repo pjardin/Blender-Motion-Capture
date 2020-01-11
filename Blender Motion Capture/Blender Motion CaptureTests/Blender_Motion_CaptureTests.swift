b8690b3ec43d6ee61235fd06258411d81f43ae69
Pascal Jardin <pjardin@me.com>
2020-01-11 08:30:08 -0800

    initilizing what i have so far
    initilizing what i have so far

---
+++ Blender Motion Capture/Blender Motion CaptureTests/Blender_Motion_CaptureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Blender_Motion_CaptureTests.swift
+//  Blender Motion CaptureTests
+//
+//  Created by Pascal Jardin on 1/11/20.
+//  Copyright © 2020 Jardin Labs. All rights reserved.
+//
+
+import XCTest
+@testable import Blender_Motion_Capture
+
+class Blender_Motion_CaptureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
